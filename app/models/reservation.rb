b55b9a66ca127d8804e6bce4bc236ced81e87296
Selim albert <selim.b33@gmail.com>
2018-05-15 11:38:25 +0200

    completed reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,6 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :house
 
-validates
+  validates :guest_number, presence: true
+  validates :reservation_date, presence: true
 end
