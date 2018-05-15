b5340c6ffaff2f4426c67fcb992c3b3bdae34567
cb <charley.beigbeder@esc-pau.net>
2018-05-15 12:04:59 +0200

    beginning of houses controller, index new and show.html.erb

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,7 @@
 class House < ApplicationRecord
   belongs_to :user
+
+  validates :name, uniqueness: true, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
 end
