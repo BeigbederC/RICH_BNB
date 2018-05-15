73b04552ddbb3cb6a87540fa66eaa723902b6f1e
Sylvain Bretaudeau <sbretaudeau@gmail.com>
2018-05-15 18:15:41 +0200

    Seed done with pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'faker'
 
-puts 'Creating 10 fake houses...'
-10.times do
+House.destroy_all
 
-  house = House.new(
-    user: User.last,
-    name:    Faker::Hobbit.location,
-    description: Faker::StarWars.wookiee_sentence,
-    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
-    avaibility: true,
-    price_per_night: (300..800).to_a.sample
-  )
-  house.save!
+
+photos = [
+  "https://i.skyrock.net/6816/19686816/pics/3146267874_1_2_jLIQNqp3.jpg",
+  "http://images.lpcdn.ca/641x427/200712/19/83437-maison-promoteur-louis-xiv-blainville.jpg",
+  "http://www.atlantico.fr/sites/atlantico.fr/files/styles/une/public/images/2014/04/article-2550623-1b1ea3d200000578-573_634x399.jpg",
+  "https://i.skyrock.net/6816/19686816/pics/3146267874_1_2_jLIQNqp3.jpg",
+  "http://images.lpcdn.ca/641x427/200712/19/83437-maison-promoteur-louis-xiv-blainville.jpg",
+  "http://www.atlantico.fr/sites/atlantico.fr/files/styles/une/public/images/2014/04/article-2550623-1b1ea3d200000578-573_634x399.jpg"
+]
+
+
+puts "Creating #{photos.count} fake houses..."
+photos.each do |photo|
+
+ house = House.new(
+   user: User.last,
+   name:    Faker::Hobbit.location,
+   description: Faker::StarWars.wookiee_sentence,
+   address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+   avaibility: true,
+   price_per_night: (300..800).to_a.sample,
+   house_picture: photo
+ )
+ house.save!
 end
 puts 'Finished!'
