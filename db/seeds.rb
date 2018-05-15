b4f2c73237f9501b8dd14a40e08173c906f9b72e
Sylvain Bretaudeau <sbretaudeau@gmail.com>
2018-05-15 15:18:46 +0200

    création du seed qui a généré des houses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 10 fake houses...'
+10.times do
+
+  house = House.new(
+    user: User.last,
+    name:    Faker::Hobbit.location,
+    description: Faker::StarWars.wookiee_sentence,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    avaibility: true,
+    price_per_night: (300..800).to_a.sample
+  )
+  house.save!
+end
+puts 'Finished!'
